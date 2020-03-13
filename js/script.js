e24c65805aeb03f944f8a238d2977b6aee3e48d6
Rafal Szymanko <szymanko@gmail.com>
2020-03-13 19:43:30 +0100

    Add script to connect links with articles

--- js/script.js
+++ js/script.js
@@ -1,18 +1,20 @@
 'use strict';
 
 const titleClickHandler = function(event){
-    console.log('Link was clicked!');
-    console.log(event);
+    // console.log('Link was clicked!');
+    // console.log(event);
+    event.preventDefault();
     const clickedElement = this;
-    console.log('Link was clicked!');
+    
     /* remove class 'active' from all article links  */
     const activeLinks = document.querySelectorAll('.titles a.active');
 
     for(let activeLink of activeLinks){
       activeLink.classList.remove('active');
     }
+
     /* add class 'active' to the clicked link */
- clickedElement.classList.add('active');
+    clickedElement.classList.add('active');
     console.log('clickedElement:', clickedElement);
 
 
@@ -21,11 +23,15 @@ const titleClickHandler = function(event){
     for (let activeArticle of activeArticles) {
         activeArticle.classList.remove('active');
     }
+
     /* get 'href' attribute from the clicked link */
+    const articleSelector = clickedElement.getAttribute("href");
   
     /* find the correct article using the selector (value of 'href' attribute) */
-  
+    const targetArticle = document.querySelector(articleSelector);
+
     /* add class 'active' to the correct article */
+    targetArticle.classList.add('active');
   }
   
   const links = document.querySelectorAll('.titles a');
