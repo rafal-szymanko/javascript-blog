4456d261cb4a5eb60c2ccd78456a2ec1a77f8d9c
Rafal Szymanko <szymanko@gmail.com>
2020-03-14 19:00:07 +0100

    Add script to generate list of titles and articles

--- js/script.js
+++ js/script.js
@@ -15,7 +15,7 @@ const titleClickHandler = function(event){
 
     /* add class 'active' to the clicked link */
     clickedElement.classList.add('active');
-    console.log('clickedElement:', clickedElement);
+    // console.log('clickedElement:', clickedElement);
 
 
     /* remove class 'active' from all articles */
@@ -26,16 +26,40 @@ const titleClickHandler = function(event){
 
     /* get 'href' attribute from the clicked link */
     const articleSelector = clickedElement.getAttribute("href");
-  
+
     /* find the correct article using the selector (value of 'href' attribute) */
     const targetArticle = document.querySelector(articleSelector);
-
     /* add class 'active' to the correct article */
     targetArticle.classList.add('active');
   }
   
-  const links = document.querySelectorAll('.titles a');
+
+  const optArticleSelector = '.post',
+  optTitleSelector = '.post-title',
+  optTitleListSelector = '.titles';
+
+function generateTitleLinks(){
+
+  /* remove contents of titleList */
+    const titleList = document.querySelector(optTitleListSelector);
+    titleList.innerHTML = '';
+    
+  /* for each article */
+    const articles = document.querySelectorAll(optArticleSelector);
+    let html = '';
+    for (let article of articles) {
+      const articleId = article.getAttribute('id');
+      const articleTitle = article.querySelector(optTitleSelector).innerHTML;
+      const linkHTML = '<li><a href="#' + articleId + '"><span>' + articleTitle + '</span></a></li>';
+      titleList.insertAdjacentHTML('beforeend', linkHTML);
+      html = html + linkHTML;
+    }
+    titleList.innerHTML = html;
+    const links = document.querySelectorAll('.titles a');
   
-  for(let link of links){
-    link.addEventListener('click', titleClickHandler);
-  }
+    for(let link of links){
+      link.addEventListener('click', titleClickHandler);
+    }
+}
+
+generateTitleLinks();
